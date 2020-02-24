65238305f1901de01493a614bcea16d822b0599a
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-02-24 09:34:45 -0800

    changed route api/cats

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
-require('dotenv').config();
+require("dotenv").config();
 // Application Dependencies
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const pg = require('pg');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const pg = require("pg");
 console.log(process.env);
 // Database Client
 const Client = pg.Client;
@@ -13,30 +12,29 @@ client.connect();
 // Application Setup
 const app = express();
 const PORT = process.env.PORT;
-app.use(morgan('dev')); // http logging
+app.use(morgan("dev")); // http logging
 app.use(cors()); // enable CORS request
-app.use(express.static('public')); // server files from /public folder
+app.use(express.static("public")); // server files from /public folder
 app.use(express.json()); // enable reading incoming json data
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.get('/api/cars', async (req, res) => {
-    try {
-        const result = await client.query(`
+app.get("/api/cats", async (req, res) => {
+  try {
+    const result = await client.query(`
             SELECT
                 c.*,
                 t.name as type
-            FROM cars c
+            FROM cats c
             JOIN types t
             ON   c.type_id = t.id
             ORDER BY c.year;
         `);
-        res.json(result.rows);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: err.message || err
-        });
-    }
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: err.message || err
+    });
+  }
 });
 // API Routes
 // app.get('/api/cars', async(req, res) => {
@@ -45,7 +43,7 @@ app.get('/api/cars', async (req, res) => {
 //             SELECT
 //             id,
 //             brand,
-//             year, 
+//             year,
 //             type,
 //             model,
 //             image,
@@ -63,5 +61,5 @@ app.get('/api/cars', async (req, res) => {
 //Get another route for a specific product (then go to create table to create another talbe )
 // Start the server
 app.listen(PORT, () => {
-    console.log('server running on PORT', PORT);
+  console.log("server running on PORT", PORT);
 });
